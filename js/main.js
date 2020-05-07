07a719c6526bece0c0742c3610928dd144d6170b
0reh <jaoreh@o2.pl>
2020-05-07 10:40:27 +0300

    my current commit

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+var btnContainer = document.getElementById("myBtnContainer2");
+var btns = btnContainer.getElementsByClassName("btn2");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("actual");
+    current[0].className = current[0].className.replace(" actual", "");
+    this.className += " actual";
+  });
+}
